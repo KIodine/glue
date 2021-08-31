20d8c1cb48b4e9ad0b650df0221c7c90fd363787
KIodine <KIodine@github.com>
2021-08-31 18:02:03 +0800

    X: tidy notes

--- glue.go
+++ glue.go
@@ -14,21 +14,21 @@ var (
 	ErrTypeIncompat = errors.New("types are not compatible")
 )
 
-/* TODO:
+/* PROPASAL:
 - [ ] cache tag analyze result?
 	Protected by rwlock?
-	- `var cacheLock sync.RWMutex`
+	- `var cacheLock sync.Mutex`
 	- `var typeCache map[reflect.Type]*fieldCache`
-	- `type fieldIdent string`
-	- `type fieldCache map[fieldIdent]fieldAttr`
+	- `type fieldCache map[string]fieldAttr`
 	```
 	type fieldAttr struct {
 		PushName string // empty string indicates ignore.
-		PullName string
+		//PullName string
 	}
 	```
 - [ ] allow push/pull fields: `glue:"pull=Alpha,push=Beta"`
-	- Allow override unexported field?
+	+ Reject the idea of `push` attr.
+	- Allow override unexported field? -> No
 - [ ] allow ignore fields, ex:
 	- ignore both: `glue:"pull=-,push=-"` or `glue:"-"`
 		```
@@ -45,7 +45,8 @@ var (
 		`glue:"push=-,pull=Arc`(export as none, pull from `Arc`)
 	- panic if: exported duplicated name, duplicated tag hint.
 - [ ] do deepcopy: `glue:"deep"`
-- [ ] allow get from method? Only methods require no parameter.
+- [ ] allow get from method? Only methods require no parameter and must have
+	matching type.
 */
 
 /* Glue copies fields from src to dst that have the same name and the same type.
@@ -62,54 +63,52 @@ func Glue(dst, src interface{}) error {
 		/* reflect stuffs */
 		dstStruct, srcStruct       reflect.Value
 		dstType, srcType           reflect.Type
-		nameSrcField, nameDstField string
+		srcFieldName, dstFieldName string
 		dstFieldMeta, srcFieldMeta reflect.StructField
 		dstField, srcField         reflect.Value
-		nameByTag                  string
+		srcTagName                 string
 	)
 	if !isValidPtrToStruct(&vdst) || !isValidPtrToStruct(&vsrc) {
 		return ErrTypeIncompat
 	}
-	dstStruct = vdst.Elem()    // reflect.Indirect(vdst)
-	dstType = dstStruct.Type() //reflect.TypeOf(dstStruct)
-	srcStruct = vsrc.Elem()    //reflect.Indirect(vsrc)
-	srcType = srcStruct.Type() //reflect.TypeOf(srcStruct)
+	dstStruct = vdst.Elem()
+	srcStruct = vsrc.Elem()
+	dstType = dstStruct.Type()
+	srcType = srcStruct.Type()
 
 	dstNumFields := dstType.NumField()
+	/* X: if do cache tag parse results, do it here and analyze all fields at
+	once, protected by mutex lock. `parseTag(v *reflect.Value) fieldCache`
+	glue tags have no effect on the src side. */
 
 	/* for each field have the same name and same type, copy value. */
 	for i := 0; i < dstNumFields; i++ {
 		dstFieldMeta = dstType.Field(i)
-		nameSrcField = dstFieldMeta.Name
-		nameDstField = nameSrcField
+		srcFieldName = dstFieldMeta.Name
+		dstFieldName = srcFieldName
 		/* only set public fields. This should be the same on the src. */
-		/* backport of method `IsExported(1.17~)` */
+		/* backport of method `IsExported(1.17-)` */
 		if !(dstFieldMeta.PkgPath == "") {
 			continue
 		}
 		/* allow gluing src fields specified by tag, this takes priority. */
 		/* X: allow strict src format "<struct>.<field>" ? */
-		nameByTag, exist = dstFieldMeta.Tag.Lookup(glueTagKey)
+		srcTagName, exist = dstFieldMeta.Tag.Lookup(glueTagKey)
 		if exist {
-			if !isValidIdentifier(nameByTag) {
+			/* X: validate only once? how? */
+			if !isValidIdentifier(srcTagName) {
 				/* if the tag is not a valid identifier, it would never had a
 				chance to be satisfied or to satisfy an untagged field. */
-				panic(fmt.Errorf("%q is not a valid identifier", nameByTag))
+				panic(fmt.Errorf("%q is not a valid identifier", srcTagName))
 			}
-			nameSrcField = nameByTag
+			srcFieldName = srcTagName
 		}
-		srcFieldMeta, exist = srcType.FieldByName(nameSrcField)
+		srcFieldMeta, exist = srcType.FieldByName(srcFieldName)
 		/* no corresponding field on src. */
 		if !exist {
 			continue
 		}
-		/* test if type equal */
-		/*
-			There are several unclear question/problems:
-			1) There are type aliases. Type does strict compare, or this is
-				exactly what we desire? -> Yes.
-			2) Can type compare directly? even they are just interfaces? -> Yes.
-		*/
+		/* test if types are strictly equal */
 		if dstFieldMeta.Type != srcFieldMeta.Type {
 			continue
 		}
@@ -118,8 +117,8 @@ func Glue(dst, src interface{}) error {
 			1) the dst field must exist
 			2) we can get the field on src by the name from dst field
 		*/
-		dstField = dstStruct.FieldByName(nameDstField)
-		srcField = srcStruct.FieldByName(nameSrcField)
+		dstField = dstStruct.FieldByName(dstFieldName)
+		srcField = srcStruct.FieldByName(srcFieldName)
 		/* require both side can set.(probably just need to test one side) */
 		/* Q: `CanSet` means `can mutate`? is there such thing a immutable field
 		in struct? */
@@ -141,7 +140,7 @@ func isValidPtrToStruct(rv *reflect.Value) bool {
 	if rv.IsNil() {
 		return false
 	}
-	ind := rv.Elem() //reflect.Indirect(*rv)
+	ind := rv.Elem()
 	/* don't care it's zero. */
 	//lint:ignore S1008 I know what I'm doing.
 	if ind.Kind() != reflect.Struct {
@@ -160,8 +159,8 @@ func isValidIdentifier(s string) bool {
 	}
 	r, sz = utf8.DecodeRuneInString(s)
 	/* golang language spec requires the first unicode character must be a
-	"Letter". */
-	if r == utf8.RuneError || !unicode.IsLetter(r) {
+	"Letter" or '_'. */
+	if r == utf8.RuneError || !unicode.IsLetter(r) || r == rune('_') {
 		/* either the string is empty and encountered an invalid utf8 encode are
 		regarded as error. */
 		return false
@@ -177,7 +176,8 @@ iter_rune:
 			}
 			return false
 		}
-		if !(unicode.IsLetter(r) || unicode.IsDigit(r) || unicode.IsNumber(r)) {
+		if !(unicode.IsLetter(r) || r == rune('_') ||
+			unicode.IsDigit(r) || unicode.IsNumber(r)) {
 			return false
 		}
 		s = s[sz:]
